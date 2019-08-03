d6276e6a4f309c4cf7a36377e08c7b86cc6796de
cenation092 <cenation092@gmail.com>
2019-08-04 04:15:55 +0530

    Login end-point added

--- backend/api.py
+++ backend/api.py
@@ -1,11 +1,15 @@
 from flask import Flask, request, jsonify
 from models import db, UserInfo
 from flask_bcrypt import Bcrypt
+from flask_bcrypt import check_password_hash
 from flask_cors import CORS
+import sqlite3
 from SendVerification import verify
 import json
 import os
+from flask_httpauth import HTTPBasicAuth
 
+auth = HTTPBasicAuth()
 app = Flask(__name__) # Flask object created
 CORS(app) # CORS (Cross-Origin Resource Sharing) is a standard for accessing web resources on different domains.
 bcrypt = Bcrypt(app) # to encrypt pasword
@@ -31,6 +35,27 @@ def create_user():
         return jsonify(new_user.toJSON()), 201
     except:
         return { "Error" : "Internal Server Error" }, 500
+
+@app.route("/api/login/", methods=["POST"] )
+def make_login():
+    data = request.json["User"]
+    Email = data["Email"]
+    Password = data["Password"]
+    exist = 0
+    Token = "NULL"
+    query_table = UserInfo.query.all()
+    for p in query_table:
+        if check_password_hash(p._password, Password) and p.email == Email:
+            exist = 1
+            break
+    if exist == 1:
+        return {"User": "Exist" }, 200
+    else:
+        return {"User":"Not Exist"}, 406
+    # try:
+        
+    # except:
+    #     return { "Error" : "Internal Server Error" }, 500
 
 with app.app_context():
     db.create_all()
--- backend/models.py
+++ backend/models.py
@@ -1,21 +1,21 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
 
 class UserInfo(db.Model):
     __tablename__ = "UserData"
     #attributes for userdata table
+    id = db.Column(db.Integer, primary_key = True )
     name = db.Column( db.String(100), nullable = False )
-    email = db.Column( db.String(120), primary_key=True )
+    email = db.Column( db.String(120), unique = True )
     _password = db.Column( db.String(100), nullable = False )
     status = db.Column( db.String(10), nullable = False ) #for verify email
     phone_number = db.Column(db.Integer,nullable = True)
     created_on = db.Column(db.DateTime(), default=datetime.utcnow)
     updated_on = db.Column(db.DateTime(), default=datetime.utcnow, onupdate=datetime.utcnow)    
 
-    def __init__(self, email, name, _password ):
+    def __init__(self, name, email, _password ):
         self.name = name
         self.email = email
         self._password = _password
